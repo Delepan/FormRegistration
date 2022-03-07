4ed1015b5a1749dd100f452bd5e2bf5356fdfa9c
Delepan Ananthan <delepan_ananthan@epam.com>
2022-03-08 01:56:31 +0800

    app file updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Container fixed className="App">
+      <Box component="form" sx={{ bgcolor: "#cfe8fc", height: "100vh" }}>
+        <div>
+          <TextField required id="outlined-required" label="Name" />
+          <TextField
+            required
+            id="outlined-required"
+            label="Password"
+            type="password"
+          />
+        </div>
+      </Box>
+    </Container>
+  );
 }
 
 export default App;
